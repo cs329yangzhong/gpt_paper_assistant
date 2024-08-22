75450f50e0be1a6d5ff9b9c81910806a3a700d74
cs329yangzhong <yang.zhong@utexas.edu>
2024-08-22 17:39:03 -0400

    3

--- filter_papers.py
+++ filter_papers.py
@@ -58,7 +58,7 @@ def calc_price(model, usage):
         return (0.03 * usage.prompt_tokens + 0.06 * usage.completion_tokens) / 1000.0
     if (model == "gpt-3.5-turbo") or (model == "gpt-3.5-turbo-1106"):
         return (0.0015 * usage.prompt_tokens + 0.002 * usage.completion_tokens) / 1000.0
-    if "gpt4o" in model:
+    if "gpt-4o" in model:
         return (0.0025 * usage.prompt_tokens + 0.01 * usage.completion_tokens) / 1000.0
 
 
